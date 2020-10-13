5c4d1275070b75b64bbb7f643a99bde1c2c716cb
Chukwuka Emi <emichukwuka@gmail.com>
2020-10-13 13:14:55 +0100

    added private chat functionality

--- server.js
+++ server.js
@@ -15,13 +15,39 @@ const io = require("socket.io")(server, {
 	pingTimeout: 120000,
 });
 
+const privateChat = io.of("/friend");
+
+privateChat.use((socket, next) => {
+	const friends = ["Chukwuka", "Justice"];
+	const user = socket.handshake.query.name;
+	const isValid = friends.includes(user);
+	if (!isValid) {
+		next(
+			new Error(
+				"You are not friends with this user. So you can't chat with him/her"
+			)
+		);
+	} else {
+		socket.user = user;
+		next();
+	}
+});
+
+privateChat.on("connection", (socket) => {
+	console.log(`< ${socket.user} connected`);
+	socket.on("chat", ({ chat }) => {
+		console.log("chat", chat);
+		privateChat.emit("message", { user: socket.user, text: chat });
+	});
+});
+
 io.on("connection", (socket) => {
 	io.use((socket, next) => {
 		if (socket.id) return next();
 		next(new Error("Socket ID not found"));
 	});
 
-	console.log("user connected");
+	console.log(">user connected");
 	socket.on("joinRoom", ({ username, room }) => {
 		const user = addUser(socket.id, username, room);
 
@@ -43,17 +69,21 @@ io.on("connection", (socket) => {
 	socket.on("chatMessage", (message) => {
 		console.log("data", message);
 		const user = getCurrentUser(socket.id);
-		io.to(user.room).emit("message", { user: user.username, text: message });
+		if (user) {
+			io.to(user.room).emit("message", { user: user.username, text: message });
+		}
 	});
 
 	socket.on("disconnect", () => {
 		console.log("user has left");
 		const user = userLeave(socket.id);
-		io.to(user.room).emit("message", {
-			user: "admin",
-			text: `${user.username} has left`,
-		});
-		socket.leave(user.room);
+		if (user) {
+			io.to(user.room).emit("message", {
+				user: "admin",
+				text: `${user.username} has left`,
+			});
+			socket.leave(user.room);
+		}
 	});
 
 	socket.on("joinFriend", ({ friendId, sender }) => {
